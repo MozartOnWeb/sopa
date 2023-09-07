ce31bf11e4ac3c2ccaedd96ef1fdc0f5727aa8ca
Mandjou Dama <79732869+MozartOnWeb@users.noreply.github.com>
2023-09-07 00:28:55 +0000

    working on top banner

---
+++ src/components/top-banner/TopBanner.tsx
@@ -0,0 +1,14 @@
+//import styles
+import styles from "./styles.module.css";
+
+type Props = {};
+
+const TopBanner = (props: Props) => {
+  return (
+    <div>
+      <p>free shipping in us for orders over $100</p>
+    </div>
+  );
+};
+
+export default TopBanner;
