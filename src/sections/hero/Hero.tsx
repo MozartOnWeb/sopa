7eb3614fd5c2235e25610768f47c0cd64e241c7f
Mandjou Dama <79732869+MozartOnWeb@users.noreply.github.com>
2023-09-08 01:33:12 +0000

    add hero-first image

--- src/sections/hero/Hero.tsx
+++ src/sections/hero/Hero.tsx
@@ -3,12 +3,17 @@ import styles from "./styles.module.css";
 
 import Image from "next/image";
 
+//import images
+import First from "@/assets/images/hero-first.jpg";
+
 type Props = {};
 
 const Hero = (props: Props) => {
   return (
     <section className={styles.container}>
-      <div className={styles.first}></div>
+      <div className={styles.first}>
+        <Image src={First} alt="first" />
+      </div>
       <div className={styles.second}></div>
       <div className={styles.third}></div>
       <div className={styles.fourth}></div>
