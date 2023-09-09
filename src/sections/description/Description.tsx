415f5701f2c88c598ef730c1964b522b5ca7ae91
Mandjou Dama <79732869+MozartOnWeb@users.noreply.github.com>
2023-09-09 14:54:50 +0000

    initialize Description section 🏗️

---
+++ src/sections/description/Description.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+//import styles
+import styles from "./styles.module.css";
+
+type Props = {};
+
+const Description = (props: Props) => {
+  return <div>Description</div>;
+};
+
+export default Description;
