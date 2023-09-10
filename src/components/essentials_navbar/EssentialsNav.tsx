9cf93525b2ea29effa536825e64be0bf66f2ba0e
Mandjou Dama <79732869+MozartOnWeb@users.noreply.github.com>
2023-09-10 13:30:28 +0000

    Essentials Nav mostly done

--- src/components/essentials_navbar/EssentialsNav.tsx
+++ src/components/essentials_navbar/EssentialsNav.tsx
@@ -13,19 +13,38 @@ const EssentialsNav = (props: Props) => {
 
   return (
     <div className={styles.main}>
-      <button>
+      <button
+        className={active === "all" ? styles.active : ""}
+        onClick={() => setActive("all")}
+      >
         all<span>72</span>
       </button>
-      <button>
+
+      <button
+        className={active === "winter collection" ? styles.active : ""}
+        onClick={() => setActive("winter collection")}
+      >
         winter collection<span>20</span>
       </button>
-      <button>
+
+      <button
+        className={active === "new arrivals" ? styles.active : ""}
+        onClick={() => setActive("new arrivals")}
+      >
         new arrivals<span>14</span>
       </button>
-      <button>
+
+      <button
+        className={active === "best sellers" ? styles.active : ""}
+        onClick={() => setActive("best sellers")}
+      >
         best sellers<span>20</span>
       </button>
-      <button>
+
+      <button
+        className={active === "flash sale" ? styles.active : ""}
+        onClick={() => setActive("flash sale")}
+      >
         flash sale<span>18</span>
       </button>
     </div>
