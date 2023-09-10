cb3752bb9a43b46e5efbfe29091e155981922d18
Mandjou Dama <79732869+MozartOnWeb@users.noreply.github.com>
2023-09-10 13:22:04 +0000

    working on essentials nav

--- src/components/essentials_navbar/EssentialsNav.tsx
+++ src/components/essentials_navbar/EssentialsNav.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useState } from "react";
 import Image from "next/image";
 
 //import styles
@@ -6,7 +9,27 @@ import styles from "./styles.module.css";
 type Props = {};
 
 const EssentialsNav = (props: Props) => {
-  return <div>EssentialsNav</div>;
+  const [active, setActive] = useState("all");
+
+  return (
+    <div className={styles.main}>
+      <button>
+        all<span>72</span>
+      </button>
+      <button>
+        winter collection<span>20</span>
+      </button>
+      <button>
+        new arrivals<span>14</span>
+      </button>
+      <button>
+        best sellers<span>20</span>
+      </button>
+      <button>
+        flash sale<span>18</span>
+      </button>
+    </div>
+  );
 };
 
 export default EssentialsNav;
--- src/sections/by_essentials/ByEssentials.tsx
+++ src/sections/by_essentials/ByEssentials.tsx
@@ -1,14 +1,18 @@
 import Image from "next/image";
 
+type Props = {};
+
 //import styles
 import styles from "./styles.module.css";
 
-type Props = {};
+//import components
+import EssentialsNav from "@/components/essentials_navbar/EssentialsNav";
 
 const ByEssentials = (props: Props) => {
   return (
     <div className={styles.main}>
-      <h4>Shop by Essentials</h4>
+      <h4 className={styles.title}>Shop by Essentials</h4>
+      <EssentialsNav />
     </div>
   );
 };
